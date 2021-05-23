a6e054c155eb5466bb05ddb4cafb9775afea471f
yoonhong98 <yoonhong998@gmail.com>
2021-05-23 23:19:25 +0800

    interfaceUpdated

---
+++ app/src/main/java/com/example/movietimeapp/NSmovie.java
@@ -0,0 +1,14 @@
+package com.example.movietimeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NSmovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_n_smovie);
+    }
+}
